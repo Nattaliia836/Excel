55c84c27d774f370cf3179ec8a75233ac33835d6
Nataliia Ponomarenko <natasha132354@gmail.com>
2020-11-02 00:15:34 +0200

    123

--- Laboratory1Tests/CalculatorTests.cs
+++ Laboratory1Tests/CalculatorTests.cs
@@ -2,43 +2,49 @@
 using Laboratory1;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Laboratory1.Tests
-{ 
-        [TestClass()]
-        public class CalculatorTests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void EvaluateTestDiv()
         {
-            [TestMethod()]
-            public void EvaluateTestExponent()
-            {
-                Calculator calculator = new Calculator(new DataGridView());
-                Assert.AreEqual(Calculator.Evaluate("0 ^ 0"), 1);
-                Assert.AreEqual(Calculator.Evaluate("1 ^ 10"), 1);
-                Assert.AreEqual(Calculator.Evaluate("10 ^ 1"), 10);
-                Assert.AreEqual(Calculator.Evaluate("10 ^ 2"), 100);
-                Assert.AreEqual(Calculator.Evaluate("10 ^ 0"), 1);
-            }
+            Calculator calculator = new Calculator(new DataGridView());
+            Assert.AreEqual(calculator.Evaluate("0 div 200"), 0);
+            Assert.AreEqual(calculator.Evaluate("200 div 5"), 40);
+            Assert.AreEqual(calculator.Evaluate("10 div 8"), 1);
+            Assert.AreEqual(calculator.Evaluate("10 div -2"), -5);
+            Assert.AreEqual(calculator.Evaluate("10 div 3"), 3);
+        }
 
-            [TestMethod()]
-            public void EvaluateTestMMin()
-            {
-                Assert.AreEqual(Calculator.Evaluate("mmin(12, 10, 5)"), 5);
-                Assert.AreEqual(Calculator.Evaluate("mmin(1.2, 10, 3)"), 1.2);
-                Assert.AreEqual(Calculator.Evaluate("mmin(1, 1, 1)"), 1);
-                Assert.AreEqual(Calculator.Evaluate("mmin(-1, 1, 0)"), -1);
-                Assert.AreEqual(Calculator.Evaluate("mmin(-(12^3), +120123, 8000000000.123123,-23)"), -1728);
-            }
-            [TestMethod()]
-            public void EvaluateTestMod()
-            {
-                Assert.AreEqual(Calculator.Evaluate("12 mod 2"), 0);
-                Assert.AreEqual(Calculator.Evaluate("13 mod 2"), 1);
-                Assert.IsTrue(Math.Abs(Calculator.Evaluate("13.1 mod 2") - 1.1) < 0.1);
-                Assert.AreEqual(Calculator.Evaluate("0 mod 2"), 0);
-                Assert.AreEqual(Calculator.Evaluate("13 mod -2"), 1);
-            }
+        [TestMethod()]
+        public void EvaluateTest()
+        {
+            Calculator calculator = new Calculator(new DataGridView());
+
+            Assert.IsTrue(calculator.Evaluate("13 = 13") == 1);
+            Assert.IsTrue(calculator.Evaluate("13 > 12") == 1);
+            Assert.IsTrue(calculator.Evaluate("12 < 13") == 1);
+            Assert.IsTrue(calculator.Evaluate("(13 = 13) > (13 < 12)") == 1);
+        }
+        [TestMethod()]
+        public void EvaluateTestMod()
+        {
+            Calculator calculator = new Calculator(new DataGridView());
+            Assert.AreEqual(calculator.Evaluate("12 mod 2"), 0);
+            Assert.AreEqual(calculator.Evaluate("13 mod 2"), 1);
+            Assert.IsTrue(Math.Abs(calculator.Evaluate("13.1 mod 2") - 1.1) < 0.1);
+            Assert.AreEqual(calculator.Evaluate("0 mod 2"), 0);
+            Assert.AreEqual(calculator.Evaluate("13 mod -2"), 1);
+        }
+    }
 }
